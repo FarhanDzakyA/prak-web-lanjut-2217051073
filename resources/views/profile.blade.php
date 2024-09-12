5f6825c107d6901123745744543bb885a1d76083
FarhanDzakyA <farhandzaky679@gmail.com>
2024-09-12 11:34:56 +0700

    Perubahan pada view profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,25 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Profile</title>
 </head>
-<body>
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td> : </td>
-            <td><?= $nama ?></td>
-        </tr>
-        <tr>
-            <td>Kelas</td>
-            <td> : </td>
-            <td><?= $kelas ?></td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td> : </td>
-            <td><?= $npm ?></td>
-        </tr>
-    </table>
+<body class="bg-black">
+    <div class="w-screen h-screen flex justify-center items-center">
+        <div class="relative w-2/5 h-4/5 group">
+            <div class="absolute -inset-1 bg-gradient-to-b from-pink-600 to-purple-600 rounded-3xl blur opacity-75 group-hover:opacity-100 transition duration-200"></div>
+            <div class="w-full h-full relative bg-black rounded-3xl leading-none flex flex-col justify-center items-center">
+                <img class="h-2/5 rounded-full" src="https://i.pinimg.com/564x/de/4a/19/de4a19f50af28e161dee0ba96d140cdd.jpg" alt="Profile Picture">
+                <p class="text-white font-mono text-3xl mt-14 font-semibold"><?= $nama ?></p>
+                <p class="text-white font-mono text-3xl mt-1 font-semibold"><?= $kelas ?></p>
+                <p class="text-white font-mono text-3xl mt-1 font-semibold"><?= $npm ?></p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
