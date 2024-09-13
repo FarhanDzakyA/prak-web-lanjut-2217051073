2fbd1841bee0d078f9c79f9c485bb6da4820b2fd
FarhanDzakyA <farhandzaky679@gmail.com>
2024-09-13 14:03:44 +0700

    Praktikum-3

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Profile</title>
+    <title>User Profile</title>
 </head>
 <body class="bg-black">
     <div class="w-screen h-screen flex justify-center items-center">
