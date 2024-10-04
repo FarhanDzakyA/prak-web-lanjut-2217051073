ab3bcffec6468eb978ea4491a47925f2a7cd775c
FarhanDzakyA <farhandzaky679@gmail.com>
2024-10-04 16:45:18 +0700

    Modul 6

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -6,13 +6,17 @@
         <div class="relative group/box">
             <div class="absolute -inset-1 bg-gradient-to-b from-pink-600 to-purple-600 rounded-3xl blur opacity-75 group-hover/box:opacity-100 transition duration-200"></div>
             <div class="w-full h-full relative bg-black rounded-3xl leading-none flex flex-col px-6 py-4">
+                <div class="flex justify-start mb-5">
+                    <a href="{{ route('user.create') }}" class="shadow-sm shadow-pink-600 border border-pink-600 text-white bg-black rounded-xl leading-none p-4 block">Tambah Pengguna Baru</a>
+                </div>
                 <table class="table-fixed text-white">
                     <thead>
                         <tr>
                             <th scope="col" class="px-3 py-2 border-r border-b">ID</th>
                             <th scope="col" class="py-2 w-52 border-r border-b">Nama</th>
                             <th scope="col" class="py-2 w-28 border-r border-b">NPM</th>
-                            <th scope="col" class="px-3 py-2 border-b">Kelas</th>
+                            <th scope="col" class="px-3 py-2 border-r border-b">Kelas</th>
+                            <th scope="col" class="px-3 py-2 border-b">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,7 +27,8 @@
                             <td class="text-center py-2 border-r border-b"><?= $user['id'] ?></td>
                             <td class="px-3 py-2 border-r border-b"><?= $user['nama'] ?></td>
                             <td class="text-center py-2 border-r border-b"><?= $user['npm'] ?></td>
-                            <td class="text-center py-2 border-b"><?= $user['nama_kelas'] ?></td>
+                            <td class="text-center py-2 border-r border-b"><?= $user['nama_kelas'] ?></td>
+                            <td class="text-center py-2 border-b"><a href="{{ route('user.show', $user->id) }}" class="text-sky-500 underline underline-offset-2">Detail</a></td>
                         </tr>
                         <?php
                             endforeach;
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,10 +11,10 @@
         <div class="relative w-2/5 h-4/5 group">
             <div class="absolute -inset-1 bg-gradient-to-b from-pink-600 to-purple-600 rounded-3xl blur opacity-75 group-hover:opacity-100 transition duration-200"></div>
             <div class="w-full h-full relative bg-black rounded-3xl leading-none flex flex-col justify-center items-center">
-                <img class="h-2/5 rounded-full" src="https://i.pinimg.com/564x/de/4a/19/de4a19f50af28e161dee0ba96d140cdd.jpg" alt="Profile Picture">
-                <p class="text-white font-mono text-3xl mt-14 font-semibold">{{ $nama }}</p>
-                <p class="text-white font-mono text-3xl mt-1 font-semibold">{{ $nama_kelas ?? 'Kelas tidak ditemukan' }}</p>
-                <p class="text-white font-mono text-3xl mt-1 font-semibold">{{ $npm }}</p>
+                <img class="h-2/5 rounded-full" src="{{ $user->foto ? asset($user->foto) : 'https://i.pinimg.com/564x/de/4a/19/de4a19f50af28e161dee0ba96d140cdd.jpg'}}" alt="Profile Picture">
+                <p class="text-white font-mono text-3xl mt-14 font-semibold">{{ $user->nama }}</p>
+                <p class="text-white font-mono text-3xl mt-1 font-semibold">{{ $user->nama_kelas ?? 'Kelas tidak ditemukan' }}</p>
+                <p class="text-white font-mono text-3xl mt-1 font-semibold">{{ $user->npm }}</p>
             </div>
         </div>
     </div>
